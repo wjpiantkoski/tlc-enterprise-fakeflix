f3b92b6b1c165b3b698c4a9c61095aa33b3785fd
wjpiantkoski <wjpiantkoski@gmail.com>
2026-01-28 09:04:05 -0300

    feat: implement video upload endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { randomUUID } from 'crypto';
+import { extname } from 'path';
+import { diskStorage } from 'multer';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +21,50 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('video')
+  @HttpCode(HttpStatus.CREATED)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'video', maxCount: 1 },
+        { name: 'thumbnail', maxCount: 1 },
+      ],
+      {
+        dest: './uploads',
+        storage: diskStorage({
+          destination: './uploads',
+          filename: (_req, file, cb) => {
+            return cb(
+              null,
+              `${Date.now()}-${randomUUID()}${extname(file.originalname)}`,
+            );
+          },
+        }),
+        fileFilter: (_req, file, cb) => {
+          if (
+            !file.mimetype.startsWith('video/mp4') &&
+            !file.mimetype.startsWith('image/jpeg')
+          ) {
+            return cb(
+              new BadRequestException(
+                'Invalid file type. Only mp4 and jpeg files are allowed.',
+              ),
+              false,
+            );
+          }
+          return cb(null, true);
+        },
+      },
+    ),
+  )
+  uploadVideo(
+    @Req() _req: Request,
+    @UploadedFiles()
+    files: { video: Express.Multer.File; thumbnail: Express.Multer.File },
+  ): string {
+    console.log(files);
+    return 'Video uploaded successfully';
   }
 }
