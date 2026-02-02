edc92cc60e56260f6de0fc1edaf8dc06d6edd3d8
wjpiantkoski <wjpiantkoski@gmail.com>
2026-02-02 09:23:47 -0300

    fix: update import path for PrismaService to use absolute path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { PrismaService } from '@src/prisma.service';
 
 @Module({
   imports: [],
