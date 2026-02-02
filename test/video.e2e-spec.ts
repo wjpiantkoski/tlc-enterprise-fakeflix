7779eefc252cad013900e74ed08c720f7358c03c
wjpiantkoski <wjpiantkoski@gmail.com>
2026-02-02 09:27:38 -0300

    test: add e2e tests for video upload validation and file type restrictions

--- test/video.e2e-spec.ts
+++ test/video.e2e-spec.ts
@@ -65,5 +65,51 @@ describe('VideoController (e2e)', () => {
           });
         });
     });
+
+    it('should throw an error if video or thumbnail is not provided', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'Test Description',
+        videoUrl: 'uploads/video.mp4',
+        thumbnailUrl: 'uploads/thumbnail.jpg',
+        sizeInKb: 1000,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message: 'Both video and thumbnail are required',
+          error: 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+    });
+
+    it('should not allow non mp4 video files', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'Test Description',
+        videoUrl: 'uploads/sample.mp3',
+        thumbnailUrl: 'uploads/thumbnail.jpg',
+        sizeInKb: 1000,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .attach('video', './test/fixtures/sample.mp3')
+        .attach('thumbnail', './test/fixtures/sample.jpg')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message: 'Invalid file type. Only mp4 and jpeg files are allowed.',
+          error: 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+    });
   });
 });
