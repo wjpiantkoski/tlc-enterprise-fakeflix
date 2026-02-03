364d89456af21760085e3bf5863ef317c6158f41
wjpiantkoski <wjpiantkoski@gmail.com>
2026-02-03 06:32:25 -0300

    feat: add video streaming endpoint and corresponding e2e tests

--- test/video.e2e-spec.ts
+++ test/video.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Video } from '@prisma/client';
 import { AppModule } from '@src/app.module';
 import { PrismaService } from '@src/prisma.service';
 import fs from 'fs';
@@ -64,6 +65,87 @@ describe('VideoController (e2e)', () => {
             duration: video.duration,
           });
         });
+    });
+
+    it('should throw an error if video or thumbnail is not provided', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'Test Description',
+        videoUrl: 'uploads/video.mp4',
+        thumbnailUrl: 'uploads/thumbnail.jpg',
+        sizeInKb: 1000,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message: 'Both video and thumbnail are required',
+          error: 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+    });
+
+    it('should not allow non mp4 video files', async () => {
+      const video = {
+        title: 'Test Video',
+        description: 'Test Description',
+        videoUrl: 'uploads/sample.mp3',
+        thumbnailUrl: 'uploads/thumbnail.jpg',
+        sizeInKb: 1000,
+        duration: 100,
+      };
+
+      await request(app.getHttpServer())
+        .post('/video')
+        .attach('video', './test/fixtures/sample.mp3')
+        .attach('thumbnail', './test/fixtures/sample.jpg')
+        .field('title', video.title)
+        .field('description', video.description)
+        .expect(HttpStatus.BAD_REQUEST)
+        .expect({
+          message: 'Invalid file type. Only mp4 and jpeg files are allowed.',
+          error: 'Bad Request',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+    });
+  });
+
+  describe('/streaming/:videoId (GET)', () => {
+    it('should stream a video', async () => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { body: sampleVideo }: { body: Video } = await request(
+        app.getHttpServer(),
+      )
+        .post('/video')
+        .attach('video', './test/fixtures/sample.mp4')
+        .attach('thumbnail', './test/fixtures/sample.jpg')
+        .field('title', 'Test Video')
+        .field('description', 'Test Description');
+
+      const fileSize = 1430145;
+      const range = `bytes=0-${fileSize - 1}`;
+
+      const response = await request(app.getHttpServer())
+        .get(`/streaming/${sampleVideo.id}`)
+        .set('Range', range)
+        .expect(HttpStatus.PARTIAL_CONTENT);
+
+      expect(response.headers['content-range']).toBe(
+        `bytes 0-${fileSize - 1}/${fileSize}`,
+      );
+      expect(response.headers['accept-ranges']).toBe('bytes');
+      expect(response.headers['content-length']).toBe(fileSize.toString());
+      expect(response.headers['content-type']).toBe('video/mp4');
+    });
+
+    it('should return 404 if video is not found', async () => {
+      await request(app.getHttpServer())
+        .get('/streaming/123')
+        .expect(HttpStatus.NOT_FOUND);
     });
   });
 });
